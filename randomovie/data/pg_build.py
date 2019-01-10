096568c324aa08e3fce0558cee18b91f8c8a8a33
akkk33 <akkk33@protonmail.com>
2019-01-11 01:12:32 +0200

    Redesigned database structure building algorithm.

---
+++ randomovie/data/pg_build.py
@@ -0,0 +1,42 @@
+#!usr/bin/env python3
+# -*- coding: utf-8; -*-
+"""
+MIT License
+Copyright (c) 2019 Amr Khamis
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+from os import environ
+from sqlite_build import default_genres
+from psycopg2 import connect
+
+db_url = environ.get('DATABASE_URL')
+if not db_url:
+    db_url = "dbname=users user=postgres"
+
+
+def create_users():
+    con = connect(db_url)
+    cursor = con.cursor()
+    cursor.execute(
+        'CREATE TABLE IF NOT EXISTS  `genres` (`id` SERIAL PRIMARY KEY , `genre` TEXT UNIQUE')
+    cursor.execute(
+        'CREATE TABLE IF NOT EXISTS  `users` (`id` SERIAL PRIMARY KEY , `uid` INTEGER UNIQUE,'
+        '`year` INTEGER, `rating` INTEGER,`last_step` TEXT) ')
+    cursor.execute(
+        'CREATE TABLE IF NOT EXISTS `user_genres` (`id` SERIAL PRIMARY KEY, `uid` INTEGER,'
+        ' `genre_id` INTEGER, foreign key(`user_id`) references users(`uid`),'
+        ' foreign key(`genre_id`) references genres(`id`) )')
